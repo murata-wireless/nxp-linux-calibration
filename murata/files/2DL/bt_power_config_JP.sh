0f1e41b3b86a5f38ae1bc736f71a2349e9b32071
Romit Chatterjee <romit.chatterjee@murata.com>
2024-12-18 21:07:24 -0600

    Created separate BT power setting script for JP

---
+++ murata/files/2DL/bt_power_config_JP.sh
@@ -0,0 +1,6 @@
+hcitool -i hci0 cmd 3F 61 00 00 01 1C 37 43 1C 00 1C 00 00 00 01 7F 05 07 08 00 00 00 00 C2 01 00 00 00 00 00 00 00 00 00
+hcitool -i hci0 cmd 0x03 0x003
+hcitool -i hci0 cmd 3f ff 64 87 10 00 FF FF FF FF 01 00 04 00 01 00 04 00
+hcitool -i hci0 cmd 0x03 0x003
+hcitool -i hci0 cmd 0x3f 0x00ee 0x01 0x05
+hcitool -i hci0 cmd 0x03 0x003
